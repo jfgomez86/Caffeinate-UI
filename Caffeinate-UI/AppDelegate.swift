01a55d2a984c6bc8e34038b1ba9e6632243eb8e6
Jose Felix Gomez <jfgomez86@gmail.com>
2019-05-04 15:19:50 -0500

    Initial Commit

---
+++ Caffeinate-UI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Caffeinate-UI
+//
+//  Created by Jose Felix Gomez on 5/4/19.
+//  Copyright © 2019 Jose Felix Gomez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
